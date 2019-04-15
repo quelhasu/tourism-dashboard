4c5a177db6ada8c3a60a8d469d2a727c7520bd17
quelhasu <uquelhas@gmail.com>
2019-04-15 16:15:45 +0200

    :sparkles: menu form

---
+++ components/menu.js
@@ -0,0 +1,16 @@
+
+const Menu = props => (
+  <div>
+    <div>
+      <form>
+        {props.children}
+      </form>
+    </div>
+    <style jsx>{`
+    
+  `}
+    </style>
+  </div>
+);
+
+export default Menu;
