a0b1b69a7240eb6893fd536b6312538bfd00cfbb
quelhasu <uquelhas@gmail.com>
2019-04-25 16:31:14 +0200

    review chart -> bar chart

---
+++ components/bar-chart.js
@@ -0,0 +1,95 @@
+import { Bar } from 'react-chartjs-2';
+
+export default class BarChart extends React.Component {
+  options = {
+    datasetFill: true,
+    legend: {
+      display: false
+    },
+    scales: {
+      xAxes: [{
+        gridLines: {
+          color: "rgba(0, 0, 0, 0)",
+        },
+        ticks: {
+          stepSize: 50
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          beginAtZero: true, callback: function (value, index, values) {
+            return value + "%";
+          },
+          stepSize: 10
+        },
+      }]
+    }
+  }
+
+  
+  chartData = (props) => {
+    this.data.labels = Object.keys(props.evolution).map(key => { return key })
+  
+    this.data.datasets[0].data = Object.keys(props.evolution).map(key => {
+      return props.evolution[key][props.year]['value']
+    })
+  
+    const colors = orderColors(this.data.labels, this.selectedColors)
+  
+    this.data.datasets[0].backgroundColor = colors
+    this.data.datasets[0].borderColor = colors
+  }
+
+  constructor(props) {
+    super(props);
+    this.data = {
+      labels:'',
+      datasets: [
+        {
+          label: props.type,
+          borderWidth: 1,
+        }
+      ]
+    }
+    this.options.scales.yAxes[0].ticks = {
+      stepSize: props.step || 10,
+      beginAtZero: true, callback: function (value, index, values) {
+        return `${value} ${props.valueType ? props.valueType : '%'}`;
+      },
+    }
+    this.selectedColors = props.colors
+    this.chartData(props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.chartData(nextProps);
+  }
+
+  render() {
+    // this
+    // Object.keys(this.props.evolution).forEach((key, i) => {
+    //   // console.log(this.props.evolution[key][this.props.year])
+    //   // console.log(i, this.props.evolution[key])
+    //   // if(i == 1) return props.evolution[year]['Ingoing']
+    // });
+    return (
+      <div>
+        <Bar
+          data={this.data}
+          width={100}
+          height={20}
+          options={this.options}
+        />
+      </div>
+    )
+  }
+
+}
+
+function orderColors(names, selectedColors) {
+  var array = [];
+  for (var i = 0; i < names.length; i++) {
+    array.push(selectedColors[names[i]]);
+  };
+  return array;
+}
