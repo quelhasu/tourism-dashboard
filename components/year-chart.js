c3d8248ec4bc440b9fb3625cc7ad3242facf76d4
quelhasu <uquelhas@gmail.com>
2019-05-20 16:58:19 +0200

    :sparkles: year chart

---
+++ components/year-chart.js
@@ -0,0 +1,103 @@
+import { Line } from 'react-chartjs-2';
+import { Omit } from '../utils/helpers'
+
+/**
+ * Create a year chart 
+ * @class YearChart
+ * 
+ * @prop {String} height - Height of the Line chart
+ * @prop {String} width - Width of the Line chart
+ * @prop {Object[]} evolution - Monthly evolution values
+ * @prop {String[]} colors - Colors array associated with name
+ * 
+ * 
+ * @extends React.Component<Props>
+ */
+export default class YearChart extends React.Component {
+    options= {
+      responsive: true,
+      maintainAspectRatio: false,
+      title: {
+        display: false,
+        text: 'Yearly evolution per region '
+      },
+      legend:{
+        labels:{
+          fontSize: 10,
+        }
+      },
+      tooltips: {
+        mode: 'index',
+        intersect: false,
+      },
+      hover: {
+        mode: 'nearest',
+        intersect: true
+      },
+      plugins: {
+        datalabels: {
+          display: false, 
+        }
+      },
+      scales: {
+        xAxes: [{
+          ticks:{
+            fontSize:10
+          },
+          display: true,
+          scaleLabel: {
+            display: false,
+            labelString: 'Year'
+          }
+        }],
+        yAxes: [{
+          ticks:{
+            fontSize:10
+          },
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Value'
+          }
+        }]
+      }
+    }
+    data = {
+      datasets: []
+    }
+
+  constructor(props) {
+    super(props);
+    this.data.datasets = chartData(props);
+    this.options.title.text += props.var
+    this.data.labels = Object.getOwnPropertyNames(
+      this.props.evolution[Object.keys(this.props.evolution)[0]]
+    ).filter(el => el.match(/^\d{4}$/))
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.data.datasets = chartData(nextProps);
+  }
+
+  render() {
+    return (
+      <div className="month-chart">
+        <Line width={this.props.width} data={this.data} options={this.options} />
+      </div>
+    )
+  }
+}
+
+function chartData(props) {
+   return Object.keys(props.evolution).map(key => {
+    let dataArr = Object.keys(props.evolution[key]).map(elKey => props.evolution[key][elKey][props.var]);
+    dataArr.pop();
+    return {
+      label: key,
+      backgroundColor: props.colors[key],
+      borderColor: props.colors[key],
+      data: dataArr,
+      fill: false
+    }
+  })
+}
