148bad137e99e5ca33bd44210ef25c4c10d206ee
quelhasu <uquelhas@gmail.com>
2019-06-06 16:03:58 +0200

    :art: year selection in menu.js

--- components/menu.js
+++ components/menu.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import { Collapse } from 'reactstrap';
+import { Nav } from 'react-bootstrap';
 
 /**
  * Create a menu with personalized content
  * @class Menu
  * 
- * @prop {String} title - Title of the menu
+ * @prop {String} title - Title of page
+ * @prop {String} description - Description of the page
+ * @prop {String} year - Current year selected
+ * @prop {String} baseUrl - Main page to redirect
+ * @prop {String} endUrl - Params query for the baseUrl
  * @prop {React.Component} children - Personalized content
  * 
  * @extends React.Component<Props>
  */
 export default class Menu extends React.Component {
+  topYear = [
+    { value: 2013, label: '2013' },
+    { value: 2014, label: '2014' },
+    { value: 2015, label: '2015' },
+    { value: 2016, label: '2016' },
+    { value: 2017, label: '2017' },
+    { value: 2018, label: '2018' },
+    { value: 2019, label: '2019' }
+  ]
 
   constructor(props) {
     super(props);
@@ -24,7 +38,7 @@ export default class Menu extends React.Component {
 
   render() {
     return (
-      <div  className="px-2">
+      <div className="px-2">
         <div className="row">
           <div className="col-md-auto">
             <h2>{this.props.title}</h2>
@@ -40,6 +54,21 @@ export default class Menu extends React.Component {
           <div className="col">
             <Collapse isOpen={this.state.collapse}>
               <div>
+                <div className="row">
+                  <div className="col">
+                    <Nav className="justify-content-center" defaultActiveKey={this.props.year}>
+                      {this.topYear.map(({ value, label }) => (
+                        <Nav.Item key={`nav-navitem-${label}`} >
+                          <Nav.Link eventKey={`${label}`}
+                            key={`nav-navitem-link${label}`} href={`/${this.props.baseUrl}/${value}${this.props.endUrl ? `/${this.props.endUrl}` : ''}`}
+                            disabled={label == this.props.year}>
+                            {label}
+                          </Nav.Link>
+                        </Nav.Item>
+                      ))}
+                    </Nav>
+                  </div>
+                </div>
                 {this.props.children}
               </div>
 
